aa432e3efd1f23fb6c7f1072806be9398256711f
BraDing25 <mcpro5589@gmail.com>
2025-05-21 19:43:41 -0500

    Update Encoder.py

--- Encoder.py
+++ Encoder.py
@@ -1,7 +1,9 @@
-import random, zlib, base64
+import random, zlib, base64, json
 import os, pandas
 from PIL import Image
 import customtkinter as ct
+
+
 
 #==========================================================
 #============== Create Window/Widgets =====================
@@ -32,7 +34,7 @@ attFont = ct.CTkFont("Tw Cen MT Condensed",8)
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"Images")
 txt = ct.CTkImage(light_image=Image.open(os.path.join(path,"txt.png")),dark_image=Image.open(os.path.join(path,"txt.png")),size=(25,25))
 csv = ct.CTkImage(light_image=Image.open(os.path.join(path,"csv.png")),dark_image=Image.open(os.path.join(path,"csv.png")),size=(25,25))
-json = ct.CTkImage(light_image=Image.open(os.path.join(path,"json.png")),dark_image=Image.open(os.path.join(path,"json.png")),size=(25,25))
+jsonI = ct.CTkImage(light_image=Image.open(os.path.join(path,"json.png")),dark_image=Image.open(os.path.join(path,"json.png")),size=(25,25))
 html = ct.CTkImage(light_image=Image.open(os.path.join(path,"html.png")),dark_image=Image.open(os.path.join(path,"html.png")),size=(25,25))
 xls = ct.CTkImage(light_image=Image.open(os.path.join(path,"xls.png")),dark_image=Image.open(os.path.join(path,"xls.png")),size=(25,25))
 
@@ -41,8 +43,18 @@ blankIm = ct.CTkImage(light_image=Image.new(mode="RGB",size=(0,0)),dark_image=Im
 #--Left Frame--
 
 optionTitle = ct.CTkLabel(optionFrame,text="Options",font=bold)
-modeTitle = ct.CTkLabel(optionFrame,text="Detected Cipher Range*",font=normal)
-modeSelection = ct.CTkLabel(optionFrame,text="None",font=normal,fg_color=("#3b8ed0","#1f6aa5"),text_color="white",corner_radius=8,width=100)
+
+infoTitle = ct.CTkLabel(optionFrame,text="Key Information*",font=normalBold)
+
+infoFrame = ct.CTkFrame(optionFrame,fg_color="transparent")
+infoFrame.columnconfigure((0,1),weight=1)
+infoFrame.rowconfigure((0,1),weight=1)
+
+klLabel = ct.CTkLabel(infoFrame,text="Key Length:   ",font=normal)
+keyLen = ct.CTkLabel(infoFrame,text="0",font=normal,fg_color=("#3b8ed0","#1f6aa5"),text_color="white",corner_radius=8,width=20)
+maxLabel = ct.CTkLabel(infoFrame,text="Max Unicode:   ",font=normal)
+maxUL = ct.CTkLabel(infoFrame,text="0",font=normal,fg_color=("#3b8ed0","#1f6aa5"),text_color="white",corner_radius=8,width=20)
+
 spaceL = ct.CTkLabel(optionFrame,text="")
 appearanceTitle = ct.CTkLabel(optionFrame,text="Set Appearance",font=normalBold)
 
@@ -68,93 +80,64 @@ progressFrame = ct.CTkFrame(programFrame,fg_color="transparent")
 #============== Create Processing Functions ===============
 #==========================================================
 
-def compress(int_list,bar = ct.CTkProgressBar):
-    #Compresses list of integers into a single string
-    array = []
-    progress = 0
-    for i in int_list:
-        array.append(int(i))
-        bar.set(progress / len(int_list))
-        progress += 1
-    int_list=array
-
-    bitstream = 0
-    progress = 0
-    for num in int_list:
-        bitstream = (bitstream << 12) | num
-        bar.set(progress / len(int_list))
-        progress += 1
-
-    byte_len = (len(int_list) * 12 + 7) // 8
-    packed_bytes = bitstream.to_bytes(byte_len, byteorder='big')
-
-    compressed = zlib.compress(packed_bytes)
-    encoded = base64.b64encode(compressed).decode('utf-8')
+#Compresses dictionary into a single string
+def compress(dic: dict):
+    json_str = json.dumps(dic,ensure_ascii=False)         # Convert dict to JSON string
+    compressed = zlib.compress(json_str.encode('utf-8')) # Compress bytes
+    encoded = base64.b64encode(compressed).decode('utf-8')  # Base64 encode to make it string-safe
     return encoded
 
-def decompress(encoded,length,bar = ct.CTkProgressBar):
-    #Decompresses string into list of integers
-    compressed = base64.b64decode(encoded.encode('utf-8'))
-    packed_bytes = zlib.decompress(compressed)
 
-    bitstream = int.from_bytes(packed_bytes, byteorder='big')
+#Decompresses string into dictionary
+def decompress(encoded: str):
+    compressed = base64.b64decode(encoded.encode('utf-8')) # Decode base64
+    json_str = zlib.decompress(compressed).decode('utf-8') # Decompress to JSON string
+    return json.loads(json_str)
 
-    num_ints = int(length)
-    int_list = []
-    progress = 0
-    for _ in num_ints:
-        int_list.append((bitstream >> (12 * (num_ints - 1))) & 0xFFF)
-        bitstream <<= 12
-        bar.set(progress / num_ints)
-        progress += 1
-    
-    progress = 0
-    final = []
-    for i in int_list:
-        final.append(str(i).zfill(len(length.strip("0"))))
-        bar.set(progress / len(int_list))
-        progress += 1
-    return final
+
 
 dicKey = dict() #This will be the future encoding key
-nums = [] #Randomized key
-compressedNums = ""
-ciLen = 0
+compressed = ""
+pad = 0
+
+#Informational Variables (for window display only)
+maxUni = 0
+dicLen = 0
+
 
 def makeKey(string,bar = ct.CTkProgressBar): #Reads file to determine how large the cipher needs to be based on unicode characters
-    n = 0
+    n = []
+    p = 0
     progress = 0
-    for i in string: #Finds max unicode character
+    for i in string:
         x = ord(i)
-        if x > n:
-            n = x
+        if x not in n: #Adds every unique unicode character to key
+            n.append(x)
+        if x > p: #Finds max unicode character
+            p = x
         bar.set(progress / len(string))
         progress += 1
-    
-    pad = len(str(n))
-    global nums, dicKey, compressedNums,ciLen
-    ciLen = n
-    modeSelection.configure(text=str(n))
+    n.sort()
+
+    global dicKey, compressed, pad, maxUni, dicLen
+    pad = len(str(p))
+    maxUni = p
+    dicLen = len(n)
+    keyLen.configure(text=str(dicLen))
+    maxUL.configure(text=str(maxUni))
 
     progress = 0
-    array = random.sample(range(n),n)
-    for i in array:
-        nums.append(str(i).zfill(pad)) #Creates a randomized, padded list of integers to based on max unicode number
-        bar.set(progress / len(array))
-        progress += 1
-    
-    bar2 = ct.CTkProgressBar(progressFrame,width=370,progress_color="green")
-    bar2.set(0)
-    bar2.pack()
-    compressedNums = compress(nums,bar2)
-    bar2.destroy()
+    rand = random.sample(range(p),len(n))
+    rand = list(map(lambda x:str(x).zfill(pad),rand))
 
-    dicKey = dict(zip(range(1,n+1),nums)) #Creates a dictionary to match characters
+    dicKey = dict(zip(n,rand)) #Creates a dictionary to match characters
+    compressed = compress(dicKey)
+
 
 #Encodes by matching each character to a randomized character and storing that key
 def encode(string,bar = ct.CTkProgressBar):
     new = ""
-    new += fileType.zfill(10) + str(ciLen).zfill(7) + str(len(compressedNums)).zfill(8) + compressedNums #Store the key
+    new += fileType.zfill(10) + str(len(compressed)).zfill(8) + str(pad) + compressed #Store the key
     progress = 0
     for char in string:
         num = ord(char)
@@ -162,32 +145,36 @@ def encode(string,bar = ct.CTkProgressBar):
         bar.set(progress / len(string))
         progress += 1
     new = encodeHex(new) #Extra layer of encription to encode the key into hexidecimal
+    global downData
+    downData = new
     return new
+
+
+downData = ""
 
 #Decodes by matching each randomized character to its normal counterpart
 def decode(string = "",bar = ct.CTkProgressBar):
     new = ""
     string = decodeHex(string) #First decode the hexidecimal encoding
-    ci = string[10:17]
-    keyLength = int(string[17:25]) #Retrieve length of the compressed randomized list
-    bar2 = ct.CTkProgressBar(progressFrame,width=370,progress_color="green")
-    bar2.set(0)
-    bar2.pack()
-    oldNums = decompress(string[25:(keyLength+25)],ci,bar2) #Decompress randomized list
-    bar2.destroy()
+    keyLength = int(string[10:18]) #Retrieve length of the compressed dictionary
+    oPad = int(string[18:19])
 
-    key = dict(zip(oldNums,range(1,int(ci)+1))) #Generate key based on old list
-    data = string[(keyLength+25):] #Retrieve the actual data to convert
+    oldNums = decompress(string[19:(keyLength+19)]) #Decompress dictionary
+    key = {v: k for k, v in oldNums.items()}
+
+    data = string[(keyLength+19):] #Retrieve the actual data to convert
     
     #Convert unicode characters back into normal script
-    pad = len(ci.strip("0"))
     progress = 0
-    for i in range(0,len(data),pad):
-        code = data[i:i+pad]
-        new += chr(key[code])
+    for i in range(0,len(data),oPad):
+        code = data[i:i+oPad]
+        new += chr(int(key[code]))
         bar.set(progress / len(data))
         progress += 1
+    global downData
+    downData = new
     return new
+
 
 #Converts string into hexidecimal by each character
 def encodeHex(string):
@@ -195,6 +182,7 @@ def encodeHex(string):
     for char in string:
         new += format(ord(char),"x").zfill(3)
     return new
+
 
 #Converts hexidecimal back into a string
 def decodeHex(string):
@@ -213,13 +201,16 @@ fileType = ""
 data = ""
 
 def modeSelect(input = ""): #Automatically selects encoding or decoding button based on file format
-    if (input[:10].find(".") != -1) and (input[10:25].isdigit):
-        selection.set("     Decode     ")
-        modeSelection.configure(text="Decoding ({})".format(ciLen))
-        detection.configure(text="File to Decode Found")
-    else:
-        selection.set("     Encode     ")
-        detection.configure(text="File to Encode Found")
+    try:
+        input = decodeHex(input)
+        if (input[:10].find(".") != -1) and (input[10:25].isdigit):
+            selection.set("     Decode     ")
+            detection.configure(text="File to Decode Found")
+        else:
+            selection.set("     Encode     ")
+            detection.configure(text="File to Encode Found")
+    except ValueError:
+        pass
 
 def openFile():
     errorLabel.configure(text="") #Clears errors/preview box
@@ -253,10 +244,10 @@ def openFile():
                 case ".csv":
                     openName.configure(image=csv,compound="left")
                 case ".json":
-                    openName.configure(image=json,compound="left")
+                    openName.configure(image=jsonI,compound="left")
                 case ".html":
                     openName.configure(image=html,compound="left")
-            with open(filePath,mode="r") as file:
+            with open(filePath,mode="r", encoding="utf-8") as file:
                 data = file.read()
                 makeKey(data,bar)
                 modeSelect(data)
@@ -267,12 +258,14 @@ def openFile():
             modeSelect(data)
         else:
             openName.configure(image=blankIm,compound="center",text="Open File to Begin") #Reset
-            modeSelection.configure(text="None")
+            keyLen.configure(text="0")
+            maxUL.configure(text="0")
             errorLabel.configure(text="File Type Not Supported")
             detection.configure(text="")
     except AttributeError:
         openName.configure(image=blankIm,compound="center",text="Open File to Begin") #Reset
-        modeSelection.configure(text="None")
+        keyLen.configure(text="0")
+        maxUL.configure(text="0")
         errorLabel.configure(text="File Not Opened")
         detection.configure(text="")
     bar.destroy()
@@ -302,20 +295,22 @@ def process(data):
 def download(input):
     errorLabel.configure(text="")
     try:
-        text = textPreview.get("1.0",'end-1c')
+        text = downData
         if  text != "":
             if selection.get() == "     Encode     ": #If file was encoded, download encoded data as .txt
                 file = ct.filedialog.asksaveasfile(defaultextension=".txt",title="Select Folder to Download",filetypes=[("Text Files",".txt")])
                 file.write(text)
             else:
+                input = decodeHex(input)
                 filetype = input[:10].lstrip("0") #If file was decoded, download decoded data in original format
                 readExtentions = [".txt",".csv",".json",".html"]
                 excelExtentions = [".xls",".xlsx",".xlsm",".xlsb"]
 
                 #Ensures file is downloaded properly by python
                 if filetype in readExtentions:
-                    file = ct.filedialog.asksaveasfile(defaultextension=filetype,title="Select Folder to Download",filetypes=[("Original File Type",filetype)])
-                    file.write(text)
+                    file = ct.filedialog.asksaveasfilename(defaultextension=filetype,title="Select Folder to Download",filetypes=[("Original File Type",filetype)])
+                    with open(file,"w",encoding="utf-8") as file:
+                        file.write(text)
                 elif filetype in excelExtentions:
                     errorLabel.configure(text = "Excel Downloading Not Yet Implemented")
                 else:
@@ -336,7 +331,7 @@ def close():
 
 def about(): #Creates a new window that gives basic information
     aboutWindow = ct.CTk()
-    aboutWindow.geometry("500x450")
+    aboutWindow.geometry("500x520")
     aboutWindow.title("About Me")
 
     line1 = "Steps to use encoding program:"
@@ -345,23 +340,26 @@ def about(): #Creates a new window that gives basic information
             "    but can still be manually changed \n" \
             "3) Press 'Run' \n" \
             "4) Download the new data by pressing 'Download'"
-    line3 = "*Cipher Range Information:"
-    line4 = "The number tells the program how long the key list \n" \
-            "should be, with the length representing the max \n" \
-            "unicode character found within the file"
-    line5 = "0-127: US ASCII (Standard Alphabet and Symbols) \n" \
+    line3 = "*Key Information:"
+    line4 = "''Key Length'' is how long the cipher key is, aka how many \n" \
+            "unique characters are present within the file. \n" \
+            "''Max Unicode'' is the highest unicode character found, which \n" \
+            "will affect the total encoded file size due to padding."
+    line5 = "Unicode Characters:"
+    line6 = "0-127: US ASCII (Standard Alphabet and Symbols) \n" \
             "128-2047: Most Latinic Alphabets (Arabic, Greek, etc.) \n" \
             "2018-65535: Additional Languages (Chinese, Japanese, etc.)** \n" \
             "65536-1114111: Other (More Asian Characters, Emojis, etc.)** \n" \
             "\n" \
-            "**!!WARNING!! Numbers in higher ranges cause the program \n" \
+            "**!!WARNING!! Large files can cause the program \n" \
             "    to slow down or even crash"
 
     label1 = ct.CTkLabel(aboutWindow,text=line1,font=normalBold)
     label2 = ct.CTkLabel(aboutWindow,text=line2,font=normal,justify="left")
     label3 = ct.CTkLabel(aboutWindow,text=line3,font=normalBold)
     label4 = ct.CTkLabel(aboutWindow,text=line4,font=normal,justify="left")
-    label5 = ct.CTkLabel(aboutWindow,text=line5,font=normal,justify="left")
+    label5 = ct.CTkLabel(aboutWindow,text=line5,font=normalBold,justify="left")
+    label6 = ct.CTkLabel(aboutWindow,text=line6,font=normal,justify="left")
     
     space = ct.CTkLabel(aboutWindow,text="")
 
@@ -371,6 +369,7 @@ def about(): #Creates a new window that gives basic information
     label3.pack(anchor="w",padx=15,pady=8)
     label4.pack(anchor="w",padx=15)
     label5.pack(anchor="w",padx=15,pady=5)
+    label6.pack(anchor="w",padx=15)
 
     aboutWindow.mainloop()
 
@@ -378,12 +377,16 @@ def about(): #Creates a new window that gives basic information
 #============== Place Left Frame (Options) ================
 #==========================================================
 
-optionTitle.pack(pady=25)
+optionTitle.pack(pady=20)
 openButton = ct.CTkButton(optionFrame,text="Open File",command=openFile,font=normal)
-openButton.pack()
+openButton.pack(pady=10)
 
-modeTitle.pack(pady=10)
-modeSelection.pack()
+infoTitle.pack()
+klLabel.grid(row=0,column=0,sticky="w")
+keyLen.grid(row=0,column=1)
+maxLabel.grid(row=1,column=0,sticky="w")
+maxUL.grid(row=1,column=1)
+infoFrame.pack(pady=10)
 
 spaceL.pack(pady=15)
 
